6f022c67fde7ee18f187b4e28861c91215c015c3
Jorge Ramos <jose.ramos@nextiva.com>
2020-11-10 10:26:02 -0600

    Disable Redux Dev Tools on Production mode

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers =
+  (
+    process.env.NODE_ENV !== 'production'
+    && typeof window !== 'undefined'
+    && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  )
+  || compose;
 
 const store = createStore(
   rootReducer,
