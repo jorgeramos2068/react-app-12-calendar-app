5a61900a6cdca8fcf96793deda44229c61c989e6
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-16 16:11:36 -0600

    Add authentication functionality through backend

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 import calendarReducer from './calendarReducers';
 import uiReducer from './uiReducer';
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   calendar: calendarReducer,
   ui: uiReducer,
 });
